49e9b7499a4d26f2361aa54fe18f8cba14c81f29
MAT-2 <mpcasey30@gmail.com>
2023-10-30 16:58:59 -0500

    Added updates so sql syntax is functional.

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,6 +3,10 @@ VALUES ("Engineering"),
        ("Finance"),
        ("Legal"),
        ("Sales");
+       ("Human Resources");
+       ("Marketing");
+       ("Customer Service");
+       ("Maintenance"),
 
 INSERT INTO roles (title, salary, department_id)
 VALUES ("Sales Lead", 100000, 1),
@@ -15,11 +19,11 @@ VALUES ("Sales Lead", 100000, 1),
        ("Lawyer", 190000, 8);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Frodo", "Baggins", 1),
-       ("Sam", "Gamgee", 2, 1),
-       ("Gandalf", "Grey", 3),
-       ("Legolas", "Greenleaf", 4, 2),
-       ("Gimli", "Glóin", 5),
-       ("Aragorn", "Elessar", 6, 3),
-       ("Peregrin", "Took", 7,),
-       ("Meriadoc", "Brandybuck", 8, 4);
+VALUES ("Frodo", "Baggins", 1, 1),
+       ("Sam", "Gamgee", 2, 2),
+       ("Gandalf", "Grey", 3, 3),
+       ("Legolas", "Greenleaf", 4, 4),
+       ("Gimli", "Glóin", 5, 5),
+       ("Aragorn", "Elessar", 6, 6),
+       ("Peregrin", "Took", 7, 7),
+       ("Meriadoc", "Brandybuck", 8, 8);
